7518e38e25a62e8dfdd44ce93c47fd493c294182
ahmaddev15 <ahmadrehmani8876@gmail.com>
2025-11-14 20:00:11 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,219 @@
+//1. Write a js program to find maximum between two numbers.
+
+/*var num1 = 10
+var num2 = 10
+
+if(num1 > num2){
+    console.log("num1",num1)
+} else if(num1 < num2){
+    console.log("num2",num2)
+} else{
+    console.log("num1 = num2")
+}*/
+
+// 2. Write a js program to find maximum between three numbers.
+
+/* var a = 88
+var b = 63
+var c = 54
+
+if(a >= b && a >= c){
+    console.log("max ", a)
+} else if(b >= a && b >= c){
+    console.log("max ", b)
+} else {
+    console.log("max ", c)
+}*/
+
+// 3. Write a js program to check whether a number is negative, positive or zero.
+
+/*
+var a = 0
+
+if(a > 0){
+    console.log("The number is positive", a)
+} else if(a < 0){
+    console.log("The number is negative", a)
+} else{
+    console.log("Zero")
+}*/
+
+// 4. Write a js program to check whether a number is divisible by 5 and 11 or not.
+
+/*
+var a = 21
+
+if(a % 5 == 0){
+    console.log("Numeber is divisible by 5")
+} else if(a % 11 == 0){
+    console.log("Numeber is divisible by 11")
+} else{
+    console.log("Numeber is not divisible by 5 and 11")
+}*/
+
+// 5. Write a js program to check whether a number is even or odd.
+
+/*
+var a = 7
+
+if (a % 2 == 0) {
+    console.log("Number is Even", a)
+} else {
+    console.log("Number is Odd", a)
+}*/
+
+// 6. Write a js program to check whether a year is leap year or not.
+
+/*
+var a = 2007
+
+if(a % 4 == 0){
+    console.log("Year is leap")
+} else {
+    console.log("Year is not leap")
+}*/
+
+// 8. Write a js program to input any alphabet and check whether it is vowel or consonant.
+
+/*
+var alphabet = 'E'
+
+if(alphabet === 'a' || alphabet === 'A'){
+    console.log("it is vowel")
+} else if(alphabet === 'e' || alphabet === 'E'){
+    console.log("it is vowel")
+} else if(alphabet === 'i' | alphabet === 'I'){
+    console.log("it is vowel")
+} else if(alphabet === 'o' || alphabet === 'O'){
+    console.log("it is vowel")
+} else if(alphabet === 'u' || alphabet === 'U'){
+    console.log("it is vowel")
+} else {
+    console.log("it is consonant")
+}*/
+
+// 10. Write a js program to check whether a character is uppercase or lowercase alphabet.
+
+/*
+var char = '0'
+
+if(char >= 'A' && char <= 'Z'){
+    console.log("character is UPPERCASE")
+} else if(char >= 'a' && char <= 'z'){
+    console.log("character is lowercase")
+} else {
+    console.log("Not an alphabet character")
+}*/
+
+// 11. Write a js program to input week number and print week day.
+
+/*
+var day = 8
+
+if(day === 1){
+    console.log("Sunday")
+} else if(day === 2){
+    console.log("Monday")
+} else if(day === 3){
+    console.log("Tuesday")
+} else if(day === 4){
+    console.log("Wednesday")
+} else if(day === 5){
+    console.log("Thursday")
+} else if(day === 6){
+    console.log("Friday")
+} else if(day === 7){
+    console.log("Saturday")
+} else {
+    console.log("only write between 1 to 7")
+}*/
+
+//12. Write a js program to input month number and print number of days in that month.
+
+/*var month = 13
+
+if (month == 1) {
+    console.log("January have 31 days")
+} else if (month == 2) {
+    console.log("Feburary have 28 days")
+} else if (month == 3) {
+    console.log("March have 31 days")
+} else if (month == 4) {
+    console.log("April have 30 days")
+} else if (month == 5) {
+    console.log("May have 31 days")
+} else if (month == 6) {
+    console.log("June have 30 days")
+} else if (month == 7) {
+    console.log("July have 31 days")
+} else if (month == 8) {
+    console.log("August have 31 days")
+} else if (month == 9) {
+    console.log("September have 30 days")
+} else if (month == 10) {
+    console.log("October have 31 days")
+} else if (month == 11) {
+    console.log("November have 30 days")
+} else if (month == 12) {
+    console.log("December have 31 days")
+} else {
+    console.log("Write Number From 1 to 12")
+}*/
+
+//18. Write a js program to calculate profit or loss.
+
+/*var costPrice = 400;    
+var sellingPrice = 300; 
+
+if (sellingPrice > costPrice) {
+    console.log("Profit: " + (sellingPrice - costPrice));
+} 
+else if (sellingPrice < costPrice) {
+    console.log("Loss: " + (costPrice - sellingPrice));
+} 
+else {
+    console.log("No Profit, No Loss");
+}*/
+
+// 19. Write a js program to input marks of five subjects Physics, Chemistry, Biology, Mathematics and Computer. 
+
+/*
+var physics = 85
+var chemistry = 40
+var biology = 70
+var math = 50
+var computer = 99
+
+var totalMarks = physics + chemistry + biology + math + computer
+console.log("Total marks:", totalMarks)
+
+var percentage = (totalMarks / 500)*100
+console.log("Percentage:", percentage)
+
+if(percentage >= 90){
+    console.log("A")
+} else if(percentage >= 80){
+    console.log("B")
+} else if(percentage >= 70){
+    console.log("C")
+} else if(percentage >= 60){
+    console.log("D")
+} else if(percentage >= 40){
+    console.log("E")
+} else {
+    console.log("Fail")
+}*/
+
+// 20. Write a js program to input basic salary of an employee and calculate its Gross salary according to following:
+
+/*
+var salary = 20000
+
+if(salary <= 10000){
+    console.log("HRA = 20%, DA = 80%")
+} else if(salary <= 20000){
+    console.log("HRA = 25%, DA = 90%")
+} else {
+    console.log("HRA = 30%, DA = 95%")
+}*/
+
